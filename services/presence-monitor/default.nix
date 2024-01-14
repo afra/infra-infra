55c8217c9af3b4f557c436f16db9ea12a2137c85
Yureka <yuka@yuka.dev>
2024-01-14 23:17:35 +0100

    presence run migrations

--- services/presence-monitor/default.nix
+++ services/presence-monitor/default.nix
@@ -74,6 +74,7 @@ in {
     environment.RUST_LOG = "trace";
     serviceConfig = {
       EnvironmentFile = config.secrets.presence-monitor-env.path;
+      ExecStartPre = "${pkgs.diesel-cli}/bin/diesel migration --migration-dir ${presence-monitor.src}/migrations --database-url /var/lib/presence-monitor/db.sqlite";
       ExecStart = "${presence-monitor}/bin/presence-monitor";
       DynamicUser = true;
       StateDirectory = "presence-monitor";
