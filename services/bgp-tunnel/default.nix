bf0ab4b1bac1f9d13214d3d4efa4a7d436c952d1
Yureka <yuka@yuka.dev>
2025-05-31 14:22:49 +0200

    simpler bgp-tunnel

--- services/bgp-tunnel/default.nix
+++ services/bgp-tunnel/default.nix
@@ -1,25 +1,8 @@
 { pkgs, config, lib, ... }:
 
-with lib;
-
 let
-  hosts = {
-    tralphium = rec {
-      magicNumber = 5;
-      wireguard = {
-        Endpoint = "[2a0f:4ac1:5::1]:51818";
-        PublicKey = "BG2oXzv2qZnQTr+PjUoEMA+vG24g617L3+5TjqHycHk=";
-      };
-    };
-    wob = rec {
-      magicNumber = 3;
-      wireguard = {
-        Endpoint = "[2a0f:4ac1:3::1]:51818";
-        PublicKey = "1BQ79VZ6UCU0TXIT5P4oLrtxqgjI19aA36ITvJQxL1s=";
-      };
-    };
-  };
-
+  ip = "2a0f:4ac0:af5a::1";
+  ip4 = "195.39.247.225";
 in {
   secrets.wireguard.owner = "systemd-network";
 
@@ -27,108 +10,66 @@ in {
 
   networking.useNetworkd = true;
 
-  networking.interfaces.lo = {
-    ipv6.addresses = [
-      { address = "2a0f:4ac0:af5a::1"; prefixLength = 128; }
-    ];
-    ipv4.addresses = [
-      { address = "195.39.247.225"; prefixLength = 32; }
-      { address = "127.0.0.53"; prefixLength = 8; }
-    ];
-  };
-
   systemd.network = {
-    netdevs = mapAttrs' (name: host: nameValuePair "40-wg-${name}" {
+    netdevs."40-wg0" = {
       netdevConfig = {
-        Name = "wg-${name}";
+        Name = "wg0";
         Kind = "wireguard";
         MTUBytes = "1500";
       };
       wireguardConfig = {
         PrivateKeyFile = config.secrets.wireguard.path;
         FirewallMark = 51820;
-        ListenPort = 18912 + host.magicNumber;
+        ListenPort = 51820;
       };
       wireguardPeers = [
         {
-          wireguardPeerConfig = host.wireguard // {
-            PersistentKeepalive = 21;
+          wireguardPeerConfig = {
             AllowedIPs = [ "0.0.0.0/0" "::/0" ];
+            PublicKey = "kih/GnR4Bov/DM/7Rd21wK+PFQRUNH6sywVuNKkUAkk=";
+            PersistentKeepalive = 21;
+            Endpoint = "[2a0f:4ac0:ca6c::1]:51820";
           };
         }
       ];
-    }) hosts;
-    networks = (
-      mapAttrs' (name: host: nameValuePair "40-wg-${name}" {
-        name = "wg-${name}";
-        addresses =  [
-          { addressConfig.Address = "fdaf::${toString host.magicNumber}:2/112"; }
-          { addressConfig.Address = "10.21.${toString host.magicNumber}.2/24"; }
-        ];
-      }) hosts
-    ) // {
-      "40-lo" = {
-        extraConfig = ''
-          # local routes
-          [RoutingPolicyRule]
-          Family=both
-          SuppressPrefixLength=0
-          Priority=1000
-
-          # send wireguard to main table
-          [RoutingPolicyRule]
-          Family=both
-          FirewallMark=51820
-          Priority=3000
-
-          # disallow any other routes for wireguard
-          [RoutingPolicyRule]
-          Family=both
-          FirewallMark=51820
-          Type=unreachable
-          Priority=3001
-
-          # local routes
-          [RoutingPolicyRule]
-          Family=both
-          Table=1
-          Priority=4000
-
-          # bgp routes
-          [RoutingPolicyRule]
-          Family=both
-          Table=2
-          Priority=5000
-        '';
-      };
     };
-  };
-
-  services.bird2 = {
-    enable = true;
-    config = fileContents ./bird2.conf + concatStrings (mapAttrsToList (name: host: ''
-      protocol bgp bgp_${name} {
-        local as 64843;
-        graceful restart on;
-
-        ipv6 {
-          igp table local6;
-          export all;
-          import filter {
-            accept;
-          };
-        };
-        ipv4 {
-          igp table local4;
-          export all;
-          import filter {
-            accept;
-          };
-        };
-
-        source address fdaf::${toString host.magicNumber}:2;
-        neighbor fdaf::${toString host.magicNumber}:1 as 207921;
-      }
-    '') hosts);
+    networks."40-wg0" = {
+      name = "wg0";
+      addresses = [
+        { Address = "${ip}/128"; }
+        { Address = "${ip4}/32"; }
+      ];
+      routes = [
+        { Gateway = "::"; Table = 51820; }
+        { Gateway = "0.0.0.0"; Table = 51820; }
+      ];
+      routingPolicyRules = [
+        {
+          # local routes
+          Family = "both";
+          SuppressPrefixLength = 0;
+          Priority = 1000;
+        }
+        {
+          # send wireguard to main table
+          Family = "both";
+          FirewallMark = 51820;
+          Priority = 3000;
+        }
+        {
+          # disallow any other routes for wireguard
+          Family = "both";
+          FirewallMark = 51820;
+          Type = "unreachable";
+          Priority = 3001;
+        }
+        {
+          # take the default route in wireguard
+          Family = "both";
+          Table = 51820;
+          Priority = 4000;
+        }
+      ];
+    };
   };
 }
