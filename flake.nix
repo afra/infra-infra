1d8a3bc17dec181ec26ef0f940dc49add2d403bf
Yureka <yuka@yuka.dev>
2026-08-28 19:58:35 +0200

    allow overriding deploy mode

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     apps.deploy = lib.pipe self.nixosConfigurations [
       (lib.mapAttrs (name: _value: (flake-utils.lib.mkApp {
         drv = pkgs.writeShellScriptBin "deploy-${name}" ''
-          ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch \
+          ${pkgs.nixos-rebuild}/bin/nixos-rebuild "''${1:-switch}" \
             --flake ${./.}#${name} \
             --target-host ${name}.afra-berlin.eu \
             --build-host ${name}.afra-berlin.eu \
